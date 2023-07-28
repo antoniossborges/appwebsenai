11c4269e5fb94b5db9231a64b804c3515e608653
Antonio Sérgio <antoniossborges@gmail.com>
2023-07-27 21:25:57 -0300

    #Adicionado entidade Conta #CRiado métodos rest para manipulação da conta

--- appwebsenai/src/main/java/com/example/appwebsenai/view/PersonView.java
+++ appwebsenai/src/main/java/com/example/appwebsenai/view/PersonView.java
@@ -5,8 +5,8 @@ import com.example.appwebsenai.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.websocket.server.PathParam;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,7 +29,7 @@ public class PersonView {
 
     @PostMapping("/person")
     public Person addPerson(@PathParam("name") String name, @PathParam("sexo") String sexo){
-       return controller.addPerson(name, sexo);
+        return controller.addPerson(name, sexo);
     }
 
     @PutMapping("/person")
