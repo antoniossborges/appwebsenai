1cb5b3d800942480c765eae81897d8d0cb975e87
Antonio Sérgio <antoniossborges@gmail.com>
2023-08-10 20:21:49 -0300

    #Primeiro frontend

---
+++ appwebsenai/src/main/resources/static/script.js
@@ -0,0 +1,8 @@
+const formPessoa = document.getElementById("cadastroPessoa");
+
+formPessoa.addEventListener("submit", function (event){
+    event.preventDefault();
+    alert("Hello World");
+
+
+});
